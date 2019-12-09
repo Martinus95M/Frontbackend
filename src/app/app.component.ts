c985229cd981653dcc26b9f960e3625f625650bb
SFPmedia <s-pauck@hotmail.com>
2019-12-09 13:47:30 +0100

    Installed 2 packages & tested new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './quiz/quiz.component.html',
+  styleUrls: ['./quiz/quiz.component.scss']
 })
 export class AppComponent {
   title = 'Frontbackend';
