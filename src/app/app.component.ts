a9c5a067e6fae6e82460bdda30c5c018bffb2429
SFPmedia <s-pauck@hotmail.com>
2019-12-13 15:15:08 +0100

    First step to D&D working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './quiz/play.component.html',
+  styleUrls: ['./quiz/play.component.scss']
 })
 export class AppComponent {
   title = 'Frontbackend';
